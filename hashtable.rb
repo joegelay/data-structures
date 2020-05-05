633615da834bd951ba0a3b5e7ce1ba1ebee0eb25
joegelay <joegelay@gmail.com>
2020-05-04 19:17:38 -0600

    add tests to hash table

--- hashtable.rb
+++ hashtable.rb
@@ -1,39 +1,48 @@
-class HashTable
-    def initialize 
-        @table = Array.new(10000, nil)
-    end 
-    
-    def store string 
-        hv = calculate_hash_value(string) 
-    
-        if @table[hv] != nil
-            @table[hv].append(string)
-        else 
-            @table[hv] = [string]
-        end 
-    end
-    
-    def lookup string
-        hv = calculate_hash_value(string)
 
-        if @table[hv] != nil
-            if @table[hv].include?(string)
-                return hv
-            end 
-        end 
-        "String not found"
+class HashTable
+
+    def initialize 
+        @table = Array.new(9122, nil)
     end 
 
     def calculate_hash_value string
         value = string[0].ord * 100 + string[1].ord
         value
     end
+
+    def store string 
+        hash_value = calculate_hash_value(string) 
+    
+        if @table[hash_value] != nil
+            @table[hash_value].append(string)
+        else 
+            @table[hash_value] = [string]
+        end 
+    end
+
+    def lookup string
+        hash_value = calculate_hash_value(string)
+
+        if @table[hash_value] != nil
+            if @table[hash_value].include?(string)
+                return hash_value
+            end 
+        end 
+        "String not found"
+    end 
 end 
 
-test = HashTable.new 
+    
+person_list = HashTable.new # => #<HashTable:0x00007fcc378f15c8 @table=[nil, nil, ...etc]
 
-p "1. #{test.calculate_hash_value("Joe")}"
-p test.store("Joe")
-p test.store("John")
-p "3. #{test.lookup("Joe")}"
-p "4. #{test.lookup("John")}"
+person_list.calculate_hash_value("Joe") # => 7511
+person_list.calculate_hash_value("Jorge") # => 7511
+person_list.calculate_hash_value("Bernard") # => 6701
+
+person_list.store("Joe") # => ["Joe"]
+person_list.store("Jorge") # => ["Joe", "Jorge"]
+person_list.store("Clifford") # => ["Clifford"]
+
+person_list.lookup("Joe") # => 7511
+person_list.lookup("Jorge") # => 7511
+person_list.lookup("Bernard") # => "String not found"
